028a0cd8fb60a2b3e99e2203667a8c38ac98a11a
KevinSilva <elmersilva462@gmail.com>
2024-09-25 18:52:35 -0600

    configuracion de la api al program

--- ProductoKDSBblazorWebAssembly/Program.cs
+++ ProductoKDSBblazorWebAssembly/Program.cs
@@ -8,4 +8,9 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
+builder.Services.AddHttpClient("JsonPlaceholder_API", client =>
+{
+    client.BaseAddress = new Uri("http://productokdsb.somee.com/");
+});
+
 await builder.Build().RunAsync();
